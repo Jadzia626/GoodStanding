ec43bc9bd63c5c816f722166549333cd65212433
Veronica K. B. Olsen <code@vkbo.net>
2017-04-01 14:46:57 +0200

    Added initial addon files

---
+++ GoodStanding.lua
@@ -0,0 +1,21 @@
+--
+--  Good Standing
+-- ===============
+--  By: Jadzia626
+--
+--  Core File
+--
+
+local GS = LibStub("AceAddon-3.0"):NewAddon("Good Standing","AceConsole-3.0")
+
+function GS:OnInitialize()
+    -- Code that you want to run when the addon is first loaded goes here.
+end
+
+function GS:OnEnable()
+    -- Called when the addon is enabled
+end
+
+function GS:OnDisable()
+    -- Called when the addon is disabled
+end
